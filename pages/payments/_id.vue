2250a3fc6f729a04ba37985f6dbf89f1996a45ce
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2022-05-16 11:25:23 +0300

    add payments and vendors category

---
+++ pages/payments/_id.vue
@@ -0,0 +1,65 @@
+<template>
+  <main>
+      <app_payment_loop 
+           :posts='data.body.posts'
+            bg='bg-strong-black'
+      />
+      <app_content 
+           :value='data.body.content' 
+           bg='bg-strong-blue'
+       />
+       <app_faq 
+           :value='data.body.faq'
+           title='Faq'
+       />
+  </main>
+</template>
+
+<script>
+    import DAL_Builder from '~/DAL/builder'
+    import app_content from '~/components/content/app-content'
+    import app_faq from '~/components/faq/app_faq'
+    import app_payment_loop from '~/components/payment_loop/app_payment_loop'
+    import config from '~/config'
+export default {
+    name: 'payment-category-page',
+    data: () => {
+        return {}
+    },
+    components: {app_content, app_faq, app_payment_loop},
+    async asyncData({route, error}) {
+        const request = new DAL_Builder()
+        const response = await request.postType('payments')
+                                      .url(route.params.id)
+                                      .get() 
+        if(response.data.confirm === 'error') {
+                 error({ statusCode: 404, message: 'Post not found' })
+        }
+        else {
+                 const body = response.data.body
+                 const data = {body}
+                 data.body.currentUrl = config.BASE_URL + route.path
+                 return {data}
+        }
+    },
+    head() {
+        return {
+            title: this.data.body.meta_title,
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: this.data.body.description
+                },
+            ],
+            link: [
+                { rel: 'canonical', href: this.data.body.currentUrl}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
